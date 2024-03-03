9c340cdd439ba77c79302b4a505a19ce7c392710
RaphyTwin <raphy-twin@outlook.com>
2024-03-03 13:08:28 +0100

    Update

--- Sharecord/Menu/MenuView.swift
+++ Sharecord/Menu/MenuView.swift
@@ -5,4 +5,55 @@
 //  Created by Raphy on 03.03.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MenuView: View {
+    
+    var body: some View {
+        
+        VStack() {
+                Button(action: {
+                    
+                }) {
+                    Text("Create File from clipboard")
+                        .padding(.vertical)
+                        .frame(width: 200)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+            
+            Button(action: {
+                
+            }) {
+                Text("Create blank File")
+                    .padding(.vertical)
+                    .frame(width: 200)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.accentColor)
+            
+            
+                Button(action: {
+                    NSApplication.shared.terminate(nil)
+                }) {
+                    Text("Quit")
+                        .foregroundStyle(Color.red)
+                        .padding(.vertical)
+                        .frame(width: 200)
+                }
+                .padding(.top)
+        }
+        .padding()
+        .frame(width: 250, height: 250)
+        .controlSize(.extraLarge)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        MenuView()
+    }
+}
--- Sharecord/SharecordApp.swift
+++ Sharecord/SharecordApp.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 @main
 struct SharecordApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var statusItem: NSStatusItem?
+    var popOver = NSPopover()
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let menuView = MenuView()
+        popOver.behavior = .transient
+        popOver.animates = true
+        popOver.contentViewController = NSViewController()
+        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
+        popOver.contentViewController?.view.window?.makeKey()
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let MenuButton = statusItem?.button {
+            
+            MenuButton.image = NSImage(systemSymbolName: "doc.text.fill", accessibilityDescription: nil)
+            MenuButton.action = #selector(MenuButtonToggle)
+        }
+    }
+    @objc func MenuButtonToggle(sender: AnyObject) {
+        
+        if popOver.isShown {
+            popOver.performClose(sender)
+        }
+        else {
+            if let menuButton = statusItem?.button {
+                
+                self.popOver.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
+            }
         }
     }
 }
