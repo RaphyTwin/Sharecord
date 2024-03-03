5a392ba0c1f6a4a36187a901db4da1d89e43c3f6
RaphyTwin <raphy-twin@outlook.com>
2024-03-03 12:44:16 +0100

    Initial Commit

---
+++ Sharecord/SharecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharecordApp.swift
+//  Sharecord
+//
+//  Created by Raphy on 03.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SharecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
