dbdd61b30be005f210e7c2792a0b68c758c081ff
RaphyTwin <126346321+RaphyTwin@users.noreply.github.com>
2024-03-09 14:23:57 +0100

    Create MenuView.swift

---
+++ Sharecord/Menu/MenuView.swift
@@ -0,0 +1,46 @@
+//
+//  MenuView.swift
+//  Sharecord
+//
+//  Created by Raphy on 03.03.24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button {
+            } label: {
+                Text("Create File from clipboard")
+                    .padding(.vertical)
+                    .frame(width: 200)
+            }
+            Button {
+            } label: {
+                Text("Create blank File")
+                    .padding(.vertical)
+                    .frame(width: 200)
+            }
+            Button {
+                NSApplication.shared.terminate(nil)
+            } label: {
+                Text("Quit")
+                    .foregroundStyle(Color.red)
+                    .padding(.vertical)
+                    .frame(width: 200)
+            }
+            .padding(.top)
+        }
+        .padding()
+        .frame(width: 250, height: 250)
+        .controlSize(.extraLarge)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
