bd2d3c401f37a14e3248d31a12339bd7b70c4ca2
RaphyTwin <126346321+RaphyTwin@users.noreply.github.com>
2024-03-09 14:25:35 +0100

    Create ContentView.swift

---
+++ Sharecord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sharecord
+//
+//  Created by Raphy on 03.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Sharecord")
+        }
+        .frame(width: 100, height: 100)
+    }
+}
+
+#Preview {
+    ContentView()
+}
