fd46aec34bd046ba6752e6ec86841f69a7aa6aa5
RaphyTwin <raphy-twin@outlook.com>
2024-03-10 17:36:05 +0100

    UI Changes (1/2)

--- Sharecord/ApplicationMenu.swift
+++ Sharecord/ApplicationMenu.swift
@@ -18,6 +18,39 @@ class ApplicationMenu: NSObject {
         customMenuItem.view = topView.view
         menu.addItem(customMenuItem)
         menu.addItem(NSMenuItem.separator())
+        
+        let aboutMenuItem = NSMenuItem(title: "About Sharecord",
+                                       action: #selector(about),
+                                       keyEquivalent: "")
+        aboutMenuItem.target = self
+        menu.addItem(aboutMenuItem)
+        
+        let settingsMenuItem = NSMenuItem(title: "Settings",
+                                       action: #selector(settings),
+                                       keyEquivalent: ",")
+        settingsMenuItem.target = self
+        menu.addItem(settingsMenuItem)
+        
+        let quitMenuItem = NSMenuItem(title: "Quit",
+                                       action: #selector(quit),
+                                       keyEquivalent: "q")
+        quitMenuItem.target = self
+        menu.addItem(quitMenuItem)
         return menu
+    }
+    
+    @objc
+    func about(sender: NSMenuItem) {
+        NSApp.orderFrontStandardAboutPanel()
+    }
+    
+    @objc
+    func settings(sender: NSMenuItem) {
+        NSApp.orderFrontStandardAboutPanel()
+    }
+    
+    @objc
+    func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
     }
 }
