ebfd3c457ccdc01bc8ebac1b4a1c12a8cd22a1e9
RaphyTwin <raphy-twin@outlook.com>
2024-03-10 18:08:09 +0100

    Added SettingsView Cannot open yet

---
+++ Sharecord/GeneralSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettingsView.swift
+//  Sharecord
+//
+//  Created by Raphy on 10.03.24.
+//
+
+import SwiftUI
+
+struct GeneralSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralSettingsView()
+}
---
+++ Sharecord/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Sharecord
+//
+//  Created by Raphy on 10.03.24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
